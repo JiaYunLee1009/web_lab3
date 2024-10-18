f8426efa84ef5d7dc2d4a7edd847f45af07694ea
JiaYunLee <lee.cammi109@gmail.com>
2024-10-18 10:40:42 +0800

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,86 @@
+var personalInfo = {
+    name: "李佳芸",
+    title: "輔仁大學醫學資訊系學生",
+    about: "你好，我是佳芸，來自台灣，目前就讀於大學，主修醫學資訊。",
+    contact: ["(+886) 908080109", "412570364@m365.fju.edu.tw", "新北市新莊區中正路510號, 242062, 台灣"],
+    skills: [],
+    certifications: []
+};
+var education = [
+    {
+        degree: "醫學資訊學士學位",
+        institution: "輔仁大學",
+        location: "新北市",
+        duration: "2023 - 現在",
+        details: [
+            "相關課程：Java物件導向程式設計、網頁設計。",
+            "畢業專題：'待定'"
+        ]
+    },
+];
+document.getElementById("name").textContent = personalInfo.name;
+document.getElementById("title").textContent = personalInfo.title;
+document.getElementById("about").textContent = personalInfo.about;
+var contactList = document.getElementById("contact-list");
+personalInfo.contact.forEach(function (contact) {
+    var li = document.createElement("li");
+    li.textContent = contact;
+    contactList.appendChild(li);
+});
+var skillsList = document.getElementById("skills-list");
+personalInfo.skills.forEach(function (skill) {
+    var li = document.createElement("li");
+    li.textContent = skill;
+    skillsList.appendChild(li);
+});
+var certificationsList = document.getElementById("certifications-list");
+personalInfo.certifications.forEach(function (certification) {
+    var li = document.createElement("li");
+    li.textContent = certification;
+    certificationsList.appendChild(li);
+});
+var experienceList = document.getElementById("experience-list");
+// experiences.forEach(exp => {
+//     const div = document.createElement("div");
+//     div.classList.add("experience-entry");
+//     const h3 = document.createElement("h3");
+//     h3.innerHTML = `<i class="fas fa-motorcycle"></i> ${exp.position}`;
+//     div.appendChild(h3);
+//     const p = document.createElement("p");
+//     p.innerHTML = `<strong>${exp.location}</strong>, ${exp.duration}`;
+//     div.appendChild(p);
+//     const ul = document.createElement("ul");
+//     exp.achievements.forEach(achievement => {
+//         const li = document.createElement("li");
+//         li.textContent = achievement;
+//         ul.appendChild(li);
+//     });
+//     if (exp.subAchievements) {
+//         exp.subAchievements.forEach(subAchievement => {
+//             const subLi = document.createElement("li");
+//             subLi.textContent = subAchievement;
+//             ul.appendChild(subLi);
+//         });
+//     }
+//     div.appendChild(ul);
+//     experienceList.appendChild(div);
+// });
+var educationList = document.getElementById("education-list");
+education.forEach(function (edu) {
+    var div = document.createElement("div");
+    div.classList.add("education-entry");
+    var h3 = document.createElement("h3");
+    h3.textContent = edu.degree;
+    div.appendChild(h3);
+    var p = document.createElement("p");
+    p.innerHTML = "<strong>".concat(edu.institution, "</strong>, ").concat(edu.location, ", ").concat(edu.duration);
+    div.appendChild(p);
+    var ul = document.createElement("ul");
+    edu.details.forEach(function (detail) {
+        var li = document.createElement("li");
+        li.textContent = detail;
+        ul.appendChild(li);
+    });
+    div.appendChild(ul);
+    educationList.appendChild(div);
+});
